950fb81a84e3d880735806e085b6e0be7cb98b10
Emanuel <evtsg@cin.ufpe.br>
2021-11-16 17:07:38 -0300

    adições e mudanças nos listeners, poucas

--- src/Player.java
+++ src/Player.java
@@ -2,27 +2,64 @@ import ui.AddSongWindow;
 import ui.PlayerWindow;
 
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class Player {
+    PlayerWindow window;
+    AddSongWindow addSongWindow;
     public Player() {
+
+        int qtdMaxMus =  100;
+        String[][] arrQueue = new String[qtdMaxMus][7];
+        String songID = "1"; //testando só
+
         ActionListener buttonListenerPlayNow = e -> {
 
         };
         ActionListener buttonListenerRemove = e -> {
 
         };
-        ActionListener buttonListernerAddSongOk = e -> {
-
-        };
         ActionListener buttonListenerPause = e -> {
 
         };
+        ActionListener buttonListenerAddSongOk = e -> {
+            arrQueue[0] = addSongWindow.getSong();
+        };
         ActionListener buttonListenerAddSong = e -> {
-            AddSongWindow addSongWindow = new AddSongWindow(null, buttonListernerAddSongOk, null);
+            addSongWindow = new AddSongWindow(songID, buttonListenerAddSongOk, window.getAddSongWindowListener());
+        };
+        MouseListener scrubberListenerClick = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
         };
 
-        PlayerWindow window = new PlayerWindow(buttonListenerPlayNow, buttonListenerRemove, buttonListenerAddSong, buttonListenerPause, null, null, null, null, null, null, null, "Player", null);
+        MouseMotionListener scrubberListenerMotion = new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
 
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {}
+        };
+        window = new PlayerWindow(buttonListenerPlayNow, buttonListenerRemove, buttonListenerAddSong, buttonListenerPause, null, null, null, null, null, scrubberListenerClick, scrubberListenerMotion, "Player", arrQueue);
     }
 }
 
