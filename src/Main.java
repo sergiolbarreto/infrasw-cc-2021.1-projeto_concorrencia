072851d2e2d8bdd6e2272cde443327b720636881
Emanuel <evtsg@cin.ufpe.br>
2021-11-15 23:15:35 -0300

    parte da carcaça dos actionListeners

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import ui.PlayerWindow;
+import ui.AddSongWindow;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
+        Player player = new Player();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,28 @@
+import ui.AddSongWindow;
+import ui.PlayerWindow;
+
+import java.awt.event.ActionListener;
+
 public class Player {
     public Player() {
+        ActionListener buttonListenerPlayNow = e -> {
+
+        };
+        ActionListener buttonListenerRemove = e -> {
+
+        };
+        ActionListener buttonListernerAddSongOk = e -> {
+            
+        };
+        ActionListener buttonListenerPause = e -> {
+
+        };
+        ActionListener buttonListenerAddSong = e -> {
+            AddSongWindow addSongWindow = new AddSongWindow(null, buttonListernerAddSongOk, null);
+        };
+
+        PlayerWindow window = new PlayerWindow(buttonListenerPlayNow, buttonListenerRemove, buttonListenerAddSong, buttonListenerPause, null, null, null, null, null, null, null, "Player", null);
+
     }
 }
 
